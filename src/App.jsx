d99d5cd8160e057f8c1159d5cefd459a89a10008
Everest Paudel <paudeleverest09@gmail.com>
2025-04-15 13:16:52 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Menubar from './components/Menubar/Menubar'
+
+function App() {
+ 
+
+  return (
+   <div>
+   <Menubar/>
+   
+   </div>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import logo from './logo.png';
+import parcel from './parcel.png';
+import upload from './upload.png';
+import cart from './cart.png';
+export const assets = {
+  logo,
+  parcel,
+  upload,
+  cart
+}
